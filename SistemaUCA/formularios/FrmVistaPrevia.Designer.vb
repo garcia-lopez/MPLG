ee8d9a5b429271244b77ee8c25d64fa80f7ae5a1
garcia-lopez <maria.lopez14959@est.uca.edu.ni>
2023-03-28 11:47:15 -0700

    prueba

--- SistemaUCA/formularios/FrmVistaPrevia.Designer.vb
+++ SistemaUCA/formularios/FrmVistaPrevia.Designer.vb
@@ -22,18 +22,49 @@ Partial Class FrmVistaPrevia
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.PruebaDS = New SistemaUCA.PruebaDS()
+        Me.PruebaDSBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CiudadBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CiudadTableAdapter = New SistemaUCA.PruebaDSTableAdapters.CiudadTableAdapter()
+        CType(Me.PruebaDS, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PruebaDSBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CiudadBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ReportViewer1
         '
         Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.CiudadBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "SistemaUCA.RptListado.rdlc"
         Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.ServerReport.BearerToken = Nothing
         Me.ReportViewer1.Size = New System.Drawing.Size(800, 450)
         Me.ReportViewer1.TabIndex = 0
+        '
+        'PruebaDS
+        '
+        Me.PruebaDS.DataSetName = "PruebaDS"
+        Me.PruebaDS.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'PruebaDSBindingSource
+        '
+        Me.PruebaDSBindingSource.DataSource = Me.PruebaDS
+        Me.PruebaDSBindingSource.Position = 0
+        '
+        'CiudadBindingSource
+        '
+        Me.CiudadBindingSource.DataMember = "Ciudad"
+        Me.CiudadBindingSource.DataSource = Me.PruebaDS
+        '
+        'CiudadTableAdapter
+        '
+        Me.CiudadTableAdapter.ClearBeforeFill = True
         '
         'FrmVistaPrevia
         '
@@ -44,9 +75,16 @@ Partial Class FrmVistaPrevia
         Me.Name = "FrmVistaPrevia"
         Me.Text = "Vista Previa"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PruebaDS, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PruebaDSBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CiudadBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents PruebaDS As PruebaDS
+    Friend WithEvents PruebaDSBindingSource As BindingSource
+    Friend WithEvents CiudadBindingSource As BindingSource
+    Friend WithEvents CiudadTableAdapter As PruebaDSTableAdapters.CiudadTableAdapter
 End Class
--- SistemaUCA/formularios/FrmVistaPrevia.vb
+++ SistemaUCA/formularios/FrmVistaPrevia.vb
@@ -1,5 +1,7 @@
 ﻿Public Class FrmVistaPrevia
     Private Sub FrmVistaPrevia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'PruebaDS.Ciudad' Puede moverla o quitarla según sea necesario.
+        Me.CiudadTableAdapter.Fill(Me.PruebaDS.Ciudad)
 
         Me.ReportViewer1.RefreshReport()
     End Sub
